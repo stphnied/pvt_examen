bec5616898749f3290280d63c7c551c7bec52ac3
steph dang <stphnie.dang@gmail.com>
2020-03-09 11:19:15 -0400

    Ajout du grid

--- category-atelier.php
+++ category-atelier.php
@@ -18,22 +18,38 @@
 <section class="grid-atelier">
     <?php
 
-    // The Query
-    $args1 = array(
-        'category_name' => 'atelier'
-    );
-    $query1 = new WP_Query($args1);
-    extraire_ateliers($query1);
+//   The Query
+while ( have_posts() ) :
+    the_post();
+
+    $nom = get_post_field('post_name');
+    $author = get_the_author_meta( 'display_name', $post->post_author );
+    $gridArea = $nom . '/' . $author . '/';
+
     
-    // The Loop
-    echo "<ol>";
-    while ($query1->have_posts()) {
-        $query1->the_post();
-        echo "<li><span >" .get_the_title()."</span>___";
-        echo "<span class='postSlug'>" .get_post_field('post_name')."</span>___";
-        echo "<span class='postAuthor'>" .get_the_author_meta( 'display_name', $post->post_author )."</<span></li>";
-    }
-    echo '</ol>';
+
+
+    echo '<p  style="grid-area:'. $gridArea .'">'.$gridArea = get_the_title().$nom.$author. '</p>';
+endwhile;
+
+    
+    // Q2
+    // The Query
+    // $args1 = array(
+    //     'category_name' => 'atelier'
+    // );
+    // $query1 = new WP_Query($args1);
+    // extraire_ateliers($query1);
+    
+    // // The Loop
+    // echo "<ol>";
+    // while ($query1->have_posts()) {
+    //     $query1->the_post();
+    //     echo "<li><span >" .get_the_title()."</span>___";
+    //     echo "<span class='postSlug'>" .get_post_field('post_name')."</span>___";
+    //     echo "<span class='postAuthor'>" .get_the_author_meta( 'display_name', $post->post_author )."</<span></li>";
+    // }
+    // echo '</ol>';
 
     // Q1
     // echo "<ol>";
